539263bed6024433ab8e1535aeea23b27558c01d
root <root@sentifine.c.sentifine-118957.internal>
2018-05-12 17:34:13 +0000

    bug-fixing

--- autonewsfeed.sh
+++ autonewsfeed.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # To retrieve news in every 5 minutes by running autonewsfeed.py
 
-printf "Job Start: %s\n" "$(date)"
+#printf "Job Start: %s\n" "$(date)"
 
-python3 /home/st118957_ait/sentifine/job/autonewsfeed.py
+#python3 /home/st118957_ait/sentifine/job/autonewsfeed.py
 
-printf "Job Finish: %s\n" "$(date)"
+#printf "Job Finish: %s\n" "$(date)"
