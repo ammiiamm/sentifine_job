2d3b1f9bf96dfc651e7744771601b27f116c0b01
ammiiamm <contact.ammii@gmail.com>
2018-05-24 03:13:08 +0700

    modif

--- autonewsfeed.sh
+++ autonewsfeed.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # To retrieve news in every 5 minutes by running autonewsfeed.py
 
-printf "Job 01_news_retrieve Start: %s\n" "$(date)"
+# printf "Job 01_news_retrieve Start: %s\n" "$(date)"
 
-python3 /home/st118957_ait/sentifine/job/scripts/python/01_news_retrieve.py
+# python3 /home/st118957_ait/sentifine/job/scripts/python/01_news_retrieve.py
 
-printf "Job 01_news_retrieve Finish: %s\n" "$(date)"
-printf "Job 02_news_extract Start: %s\n" "$(date)"
+# printf "Job 01_news_retrieve Finish: %s\n" "$(date)"
+# printf "Job 02_news_extract Start: %s\n" "$(date)"
 
-python3 /home/st118957_ait/sentifine/job/scripts/python/02_news_extract.py
+# python3 /home/st118957_ait/sentifine/job/scripts/python/02_news_extract.py
 
-printf "Job 02_news_extract Finish: %s\n" "$(date)"
-printf "Job 03_news_transform Start: %s\n" "$(date)"
+# printf "Job 02_news_extract Finish: %s\n" "$(date)"
+# printf "Job 03_news_transform Start: %s\n" "$(date)"
 
-python3 /home/st118957_ait/sentifine/job/scripts/python/03_news_transform.py
+# python3 /home/st118957_ait/sentifine/job/scripts/python/03_news_transform.py
 
-printf "Job 03_news_transform Finish: %s\n" "$(date)"
-printf "Job 04_news_load Start: %s\n" "$(date)"
+# printf "Job 03_news_transform Finish: %s\n" "$(date)"
+# printf "Job 04_news_load Start: %s\n" "$(date)"
 
-python3 /home/st118957_ait/sentifine/job/scripts/python/04_news_load.py
+# python3 /home/st118957_ait/sentifine/job/scripts/python/04_news_load.py
 
-printf "Job 04_news_load Finish: %s\n" "$(date)"
+# printf "Job 04_news_load Finish: %s\n" "$(date)"
 
-sleep 5m
+# sleep 5m
